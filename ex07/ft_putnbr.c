a991b6c671c71ce96fa55620e991b0d9b2993735
Manuel Navarro toledo <manavarr@c1r11s3.42malaga.com>
2022-03-22 14:55:18 +0100

    todos-8-OK

--- ex07/ft_putnbr.c
+++ ex07/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: manavarr <manavarr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 20:41:43 by manavarr          #+#    #+#             */
-/*   Updated: 2022/03/21 21:10:07 by manavarr         ###   ########.fr       */
+/*   Updated: 2022/03/22 14:46:21 by manavarr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,26 +19,14 @@ void	ft_write(int c)
 
 void	ft_putnbr(int nb)
 {
-	int	none;
-	int	count;
-
 	if (nb < 0)
 	{
 		ft_write('-');
 		nb *= -1;
 	}
-	none = nb;
-	count = 1;
-	while (none > 10)
+	if (nb > 10)
 	{
-		none /= 10;
-		count *= 10;
-	}
-	while (nb > 10)
-	{
-		ft_write((nb / count) + '0');
-		nb %= count;
-		count /= 10;
+		ft_putnbr(nb / 10);
 	}
 	ft_write((nb % 10) + '0');
 }
